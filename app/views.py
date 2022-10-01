dbb939ca7139d445c25ce9ee3387523d022549d7
tajriba <quvvatullayev@gmail.com>
2022-10-01 20:17:05 +0300

    fix

--- app/views.py
+++ app/views.py
@@ -7,3 +7,21 @@ def sum_num(requets):
     dict = json.loads(a)
     r = JsonResponse({'sum':dict['a'] + dict['b']})
     return r
+
+def sum_num(requets):
+    a = requets.body.decode()
+    dict = json.loads(a)
+    r = JsonResponse({'sum':dict['a'] + dict['b']})
+    return r
+
+def helov(requets):
+    return HttpResponse('<h1>Hellov</h1>')
+
+def Hom(requets):
+    return HttpResponse('<h1>Hom</h1>')
+
+def abaut(requets):
+    return HttpResponse('<h1>Abaut</h1>')
+
+def uy(requets):
+    return HttpResponse('<h1>Uy</h1>')
